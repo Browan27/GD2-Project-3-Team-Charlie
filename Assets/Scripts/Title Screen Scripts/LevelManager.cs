5837b1ecac92879c8bda5e2c27fea0d8a47d9861
Ashley N <Ashley N>
2016-11-02 08:08:48 -0500

    Added Oil Splat, 1 Sound

---
+++ Assets/Scripts/Title Screen Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class LevelManager : MonoBehaviour {
+
+
+	public void LoadMenu(string name)
+    {
+		UnityEngine.SceneManagement.SceneManager.LoadScene(name) ;
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+    }
+
+	public void HowToPlay(string name)
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("HowToPlay");
+	}
+
+	public void ExitBacktoMenu (string name)
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("TitleScreen");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/Title Screen Scripts/MusicPlaylist.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlaylist : MonoBehaviour {
+
+	public AudioClip[] clips;
+	private AudioSource audioSource;
+    private GlobalController GC;
+
+	void Start () {
+        GC = GameObject.Find("GameController").GetComponent<GlobalController>();
+        audioSource = GetComponent<AudioSource>();
+		audioSource.loop = false;
+
+	}
+
+	private AudioClip GetRandomClip() {
+		return clips [Random.Range (0, clips.Length)];
+	}
+
+	//Update is called once per frame
+	void Update() {
+
+        if (GC.gameOver) {
+            audioSource.Stop();
+        } else if (!audioSource.isPlaying) {
+			audioSource.clip = GetRandomClip ();
+			audioSource.Play ();
+		}
+
+	}
+
+}
