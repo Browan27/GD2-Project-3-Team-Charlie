1904d56a038adcdf53a2f23305917a0e07489419
Browan27 <Browan27@yahoo.com>
2016-11-28 10:36:38 -0600

    adds in counter to player selection screen.

---
+++ Assets/Scripts/Title Screen Scripts/SetText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SetText : MonoBehaviour {
+    
+    private Text data;
+    private GameObject DC;
+
+	// Use this for initialization
+	void Start () {
+        data = GetComponent<Text>();
+        DC = GameObject.Find("DataController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        data.text = "" + DC.GetComponent<DataController>().getPlayers();
+	}
+}
